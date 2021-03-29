7e2ec2d377f790107cfaf2072ccd63bc28dd7894
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2021-03-28 23:54:03 -0700

    ISSBot now correctly returns & formats data

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
-"""GW2 Bot for Discord"""
+"""ISS Bot for Discord"""
 import os
+from datetime import datetime
 from discord.ext import commands
 from dotenv import load_dotenv
-import sys
-from datetime import datetime
-import random
+import reverse_geocoder as rg
+import requests
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-
 
 bot = commands.Bot(command_prefix="!")
 
@@ -19,31 +18,35 @@ async def on_ready():
     print(f"{bot.user.name} has connected to Discord")
 
 
-@bot.command(name="ping")
-async def ping_pong(ctx):
-    """Respond to Ping with Pong"""
-    response = "pong!"
-    await ctx.send(response)
+@bot.command(name="ISS")
+async def find_iss(ctx):
+    """Returns the location of the ISS"""
+    url = "http://api.open-notify.org/iss-now.json"
+    res = requests.get(url).json()
+    time = datetime.utcfromtimestamp(res['timestamp']).strftime('%x %X')
+    latitude = res['iss_position']['latitude']
+    longitude = res['iss_position']['longitude']
+    position = rg.search((latitude, longitude))[0]
+    fmt_position = (f"{position['name']}, {position['admin1']}")
+    msg = f"""
+    ```
+    Time: {time}
+    Position:
+        Latitude: {latitude}
+        Longitude: {longitude}
+        Nearest City: {fmt_position}
+    ```
+    """
+    await ctx.send(msg)
 
 
-
-@bot.event
-async def on_error(event, *args, **kwargs):
-    """Handle errors"""
-    with open("err.log", "a") as f:
-        if event == 'on_message':
-            f.write(f"UNHANDLED MESSAGE ({datetime.now()}): {args[0]}\n"
-                    f"Exception info: {sys.exc_info()}\n")
-        else:
-            raise
-
-@bot.command(name="roll")
-async def roll_dice(ctx, number_of_dice: int, number_of_sides: int):
-    """Roll the dice!"""
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
+#@bot.event
+#async def on_error(event, *args, **kwargs):
+#    """Handle errors"""
+#    with open("err.log", "a") as f:
+#        if event == 'find_iss':
+#            f.write(f"--UNHANDLED MESSAGE-- {args[0]}\n")
+#        else:
+#            raise
 
 bot.run(TOKEN)
